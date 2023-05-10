d8a577f41562153b53673e44ae91f46e07fc9db5
RJ2607 <rishijain2607@gmail.com>
2023-05-10 21:31:24 +0530

    app bar

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:project/screens/home_screen.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
   }
 }
