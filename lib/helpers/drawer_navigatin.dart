382a04398942e2559fc34ba76e07b87d962776dc
RJ2607 <rishijain2607@gmail.com>
2023-05-10 22:07:23 +0530

    drawer navigation

--- lib/helpers/drawer_navigatin.dart
+++ lib/helpers/drawer_navigatin.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:project/screens/home_screen.dart';
 
 class DrawerNavigation extends StatefulWidget {
   @override
@@ -16,11 +17,24 @@ class _DrawerNavigationState extends State<DrawerNavigation> {
           children: [
             UserAccountsDrawerHeader(
               currentAccountPicture: CircleAvatar(
-                backgroundImage: NetworkImage("https://ibb.co/Hxn5prY"),
+                backgroundImage:
+                    NetworkImage("https://i.ibb.co/9VsQ3q2/990130.jpg"),
               ),
               accountName: Text("Rishi Jain"),
               accountEmail: Text("rishijain2607@gmail.com"),
               decoration: BoxDecoration(color: Colors.blue),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text("HOME"),
+              onTap: () => Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (context) => HomeScreen())),
+            ),
+            ListTile(
+              leading: Icon(Icons.view_list),
+              title: Text("CATEGORIES"),
+              onTap: () => Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (context) => CategoriesScreen()),
             )
           ],
         ),
