72d4e10426b00294e10e32db3da0d2257859ffce
RJ2607 <rishijain2607@gmail.com>
2023-05-14 21:49:22 +0530

    s

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -23,7 +23,7 @@ class _CategoriesScreenState extends State<CategoriesScreen> {
       body: Center(child: Text("Welcome to Categorize Screen")),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
-        child: Icon(Icons.add),
+        child: Icon(Icons.ad),
       ),
     );
   }
